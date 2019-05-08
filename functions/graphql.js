bbafcd632d386d84db331490f8ccb99ab1f2fb25
luandevpro <tranluan1c@gmail.com>
2019-05-08 22:12:32 +0700

    test

--- functions/graphql.js
+++ functions/graphql.js
@@ -4,8 +4,6 @@ const {
  } = require('apollo-server-express');
 const express = require("express");
 const serverless = require("serverless-http");
-const expressPlayground = require('graphql-playground-middleware-express')
-  .default
 
 const app = express();
 const router = express.Router();
@@ -28,11 +26,9 @@ const apollo = new ApolloServer({
   typeDefs,
   resolvers
 })
-// router.get("/playground", expressPlayground({ endpoint: "/graphql" }));
-const path = '/.netlify/functions/graphql';
 
 app.use('/.netlify/functions/graphql', router);  // path must route to lambda
 
-apollo.applyMiddleware({ app , path });
-module.exports = app;
+apollo.applyMiddleware({ app , path: '/.netlify/functions/graphql' });
+
 module.exports.handler = serverless(app);
