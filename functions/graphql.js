c47ba2868037408e48e8c4756196578b5b52227c
luandevpro <tranluan1c@gmail.com>
2019-05-08 21:19:36 +0700

    fc

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,22 +1,19 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-const resolvers = {
-  Query: {
-    hello: (root, args, context) => {
-      return `Hello from Netlify function. https://bit.ly/2UXh0fD`
-    }
-  }
-}
+app.use(bodyParser.json());
+app.use('/.netlify/functions/graphql', router);  // path must route to lambda
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
-
-exports.handler = server.createHandler()
+module.exports = app;
+module.exports.handler = serverless(app);
