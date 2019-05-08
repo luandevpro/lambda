7199a9f909236578db2730d91a4cf055bed156bd
luandevpro <tranluan1c@gmail.com>
2019-05-08 21:35:31 +0700

    test

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,18 +1,37 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const {
+   ApolloServer,
+   gql
+ } = require('apollo-server-express');
+const express = require("express");
+const serverless = require("serverless-http");
+const expressPlayground = require('graphql-playground-middleware-express')
+  .default
+
 const app = express();
-const bodyParser = require('body-parser');
-
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (root, args, context) => {
+      return `Hello from Netlify function. https://bit.ly/2UXh0fD`
+    }
+  }
+}
+
+const apollo = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+router.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+apollo.applyMiddleware({ app });
+
 app.use('/.netlify/functions/graphql', router);  // path must route to lambda
 
 module.exports = app;
